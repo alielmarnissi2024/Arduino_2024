a7de3f90bdd040128896cccd26802c522b69df0f
alielmarnissi2024 <157708123+alielmarnissi2024@users.noreply.github.com>
2024-02-18 18:23:39 +0100

    Create Ex7part1.ino

---
+++ 005 array de  8 leds/Ex7part1/Ex7part1.ino
@@ -0,0 +1,28 @@
+
+
+/**********************************************************************************
+**                              Array de 8 LEDs                                  **
+**********************************************************************************/
+//********** Includes *************************************************************
+//********** Variables ************************************************************
+const byte led0 = 5;          // donar nom al pin 5 de l’Arduino
+int i=0;
+void setup()
+{
+ 
+  pinMode(led0, OUTPUT);
+  while(i<30) {
+  digitalWrite(led0, HIGH);
+  delay(200);
+  digitalWrite(led0,LOW);
+  delay(200);
+  i++;
+  }
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
