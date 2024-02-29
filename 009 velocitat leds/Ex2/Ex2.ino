1545ec0a23b4f8b9e4b43d6e96127d91ef131b5e
alielmarnissi2024 <157708123+alielmarnissi2024@users.noreply.github.com>
2024-02-29 17:33:42 +0100

    ex1,2

---
+++ 009 velocitat leds/Ex2/Ex2.ino
@@ -0,0 +1,78 @@
+const byte led0 = 5;
+const byte led1 = 6;
+const byte led2 = 7;
+const byte led3 = 8;
+const byte led4 = 9;
+const byte led5 = 10;
+const byte led6 = 11;
+const byte led7 = 12;
+const byte pot0 = A0;
+
+unsigned long velocitat = 20;
+int valPot0;
+
+void setup()
+{
+  pinMode(led0, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  valPot0 = analogRead(pot0);
+  velocitat = 10 + valPot0; 
+  digitalWrite(led0, HIGH);
+  digitalWrite(led7, HIGH);
+  delay(velocitat);
+   
+  digitalWrite(led1, HIGH);
+  digitalWrite(led6, HIGH);
+  delay(velocitat);
+  
+  digitalWrite(led0, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led7, LOW);
+  delay(velocitat);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  delay(velocitat);
+  
+  digitalWrite(led2, LOW);
+  digitalWrite(led5, LOW);
+  
+  delay(velocitat*2);
+  
+  
+  digitalWrite(led2, HIGH);
+  digitalWrite(led5, HIGH);
+  
+  delay(velocitat);
+  
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led1, HIGH);
+  delay(velocitat);
+
+ digitalWrite(led2, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led0, HIGH);
+  digitalWrite(led7, HIGH);
+  delay(velocitat);
+ digitalWrite(led1, LOW);
+  digitalWrite(led6, LOW);
+  delay(velocitat);
+}
+  
+  
