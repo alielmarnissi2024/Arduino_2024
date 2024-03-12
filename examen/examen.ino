73f7a955aaef5ee7d335275a30d747734fa48902
alielmarnissi2024 <157708123+alielmarnissi2024@users.noreply.github.com>
2024-03-12 13:47:25 +0100

    Update examen.ino

--- examen/examen.ino
+++ examen/examen.ino
@@ -5,11 +5,9 @@ const byte led3 = 9;          // donar nom al pin 9 de l’Arduino
 const byte led4 = 10;         // donar nom al pin 10 de l’Arduino
 const byte led5 = 11;         // donar nom al pin 11 de l’Arduino
 unsigned long velocitat = 200;         // velocitat de l'acció en ms
+float valEntrada;
+float voltage;
 
- 
-
-
-//********** Setup ****************************************************************
 void setup()
 {
   Serial.begin(9600);
@@ -19,23 +17,28 @@ void setup()
   pinMode(led3, OUTPUT);     // definir el pin 9 com una sortida
   pinMode(led4, OUTPUT);     // definir el pin 10 com una sortida
   pinMode(led5, OUTPUT);     // definir el pin 11 com una sortida
-  int valEntrada = analogRead(A0);
-float voltage;
+  
 
 }
 
 
 void loop()
 {
-  int valEntrada = analogRead(A0);
-float voltage;
-
+  valEntrada = 0;
+  for (int i =0; i<100; i++)
+  {
+    valEntrada = analogRead(A0) + valEntrada;
+  }
+  valEntrada = valEntrada/100;
   voltage = ( (float) valEntrada * 5.0 ) / 1023;
+  Serial.println(voltage);
+  
   
   
   if (voltage<=1)
   { 
     analogWrite(led0, voltage*255); 
+    analogWrite(led1, LOW);
   }
   
    
@@ -43,18 +46,23 @@ float voltage;
   { 
     digitalWrite(led0, HIGH);
     analogWrite(led1,(voltage-1)*255);
+    analogWrite(led2, LOW);
+   
+
   }
     
    
-           if (voltage>=2 && voltage<3 )
+   if (voltage>=2 && voltage<3 )
   { 
     digitalWrite(led1, HIGH);
     analogWrite(led2,(voltage-2)*255);
+    analogWrite(led3, LOW);
   }
  if (voltage>=3 && voltage<4 )
   { 
     digitalWrite(led2, HIGH);
     analogWrite(led3,(voltage-3)*255);
+    analogWrite(led4, LOW);
   }
     if (voltage>=4 && voltage<5 )
   { 
